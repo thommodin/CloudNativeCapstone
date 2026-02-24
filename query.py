3537bd3229d903daf5e51fd47976bea590c62a0d
Tom Galindo <98626996+thommodin@users.noreply.github.com>
2026-02-24 11:10:38 +1100

    delete query

--- query.py
+++
@@ -1,8 +0,0 @@
-import pystac_client
-
-client = pystac_client.Client.open("catalog/catalog.json")
-print(client)
-
-search = client.search(
-    bbox=[94.921875,-31.728167,111.005859,-18.646245],
-)
